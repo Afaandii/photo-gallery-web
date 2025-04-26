3a8716b2b5ac2472f319aa04847558b841aad553
Afaandii <fandisantuy140@gmail.com>
2025-04-26 23:12:28 +0700

    perubahan pada register

--- frontend-gallery-web/src/components/Register.jsx
+++ frontend-gallery-web/src/components/Register.jsx
@@ -38,8 +38,8 @@ export default function Register() {
   };
   return (
     <>
-      <div className="min-h-screen bg-[#FF85C2] flex items-center justify-center p-4">
-        <div className="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
+      <div className="register min-h-screen bg-black bg-opacity-50 flex items-center justify-center p-4">
+        <div className="max-w-md w-full border border-slate-800 rounded-xl shadow-xl p-8">
           <h2 className="text-2xl font-bold text-gray-900 mb-6 text-center">
             Registrasi
           </h2>
@@ -51,7 +51,7 @@ export default function Register() {
               </label>
               <input
                 type="text"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
+                className="w-full px-4 py-2 border bg-transparent placeholder:text-slate-800 border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
                 placeholder="Buat Name anda"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
@@ -64,7 +64,7 @@ export default function Register() {
               </label>
               <input
                 type="email"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
+                className="w-full px-4 py-2 bg-transparent placeholder:text-slate-800 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
                 placeholder="Buat Email Kamu "
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -72,12 +72,12 @@ export default function Register() {
             </div>
 
             <div>
-              <label className="block text-sm font-medium text-gray-700 mb-1">
+              <label className="block text-sm font-medium text-gray-800 mb-1">
                 Password
               </label>
               <input
                 type="password"
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
+                className="w-full px-4 py-2 border bg-transparent placeholder:text-slate-800  border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 outline-none transition-all"
                 placeholder="Buat Password anda"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -94,8 +94,10 @@ export default function Register() {
           <div className="mt-6 text-center text-sm text-gray-600">
             Sudah memiliki akun?
             <a
-              href="/login"
-              className="text-indigo-600 hover:text-indigo-500 font-medium"
+              onClick={() =>
+                (document.location.href = "http://localhost:8000/admin")
+              }
+              className="text-indigo-600 cursor-pointer hover:text-indigo-500 font-medium"
             >
               Login
             </a>
