4293a61238b1887cc3f659c57faee0ea7d1ad06a
Afaandii <fandisantuy140@gmail.com>
2025-09-02 11:45:57 +0700

    fetch api dari backend untuk card image

--- frontend-gallery-web/src/components/Card.jsx
+++ frontend-gallery-web/src/components/Card.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Card() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/api/posts");
+
+        if (!res.ok) {
+          throw Error(`HTTP Error Status: ${res.status}`);
+        }
+
+        let data = await res.json();
+        setPosts(data.post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPost();
+  }, []);
+
   return (
     <>
       <section className="max-w-7xl mx-auto px-4 py-8">
@@ -7,35 +29,17 @@ export default function Card() {
           beberapa orang.
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <div className="overflow-hidden rounded-xl shadow-lg">
-            <a href="/">
-              <img
-                src="https://cdn.pixabay.com/photo/2023/12/06/14/39/snow-8433815_1280.jpg"
-                alt="Leaves with snow"
-                className="w-full h-[500px] object-cover"
-              />
-            </a>
-          </div>
-
-          <div className="overflow-hidden rounded-xl shadow-lg">
-            <a href="/">
-              <img
-                src="https://images.unsplash.com/photo-1574158622682-e40e69881006"
-                alt="Puppy"
-                className="w-full h-[500px] object-cover"
-              />
-            </a>
-          </div>
-
-          <div className="overflow-hidden rounded-xl shadow-lg">
-            <a href="/">
-              <img
-                src="https://cdn.pixabay.com/photo/2022/11/14/14/13/lake-7591750_1280.jpg"
-                alt="Wedding"
-                className="w-full h-[500px] object-cover"
-              />
-            </a>
-          </div>
+          {posts.map((post, index) => (
+            <div key={index} className="overflow-hidden rounded-xl shadow-lg">
+              <a href="/">
+                <img
+                  src={`http://localhost:8000/storage/${post.image}`}
+                  alt="Leaves with snow"
+                  className="w-full h-[500px] object-cover"
+                />
+              </a>
+            </div>
+          ))}
         </div>
       </section>
     </>
