47b866e4f05503fc3777789b40cc18a8db2da739
Afaandii <fandisantuy140@gmail.com>
2025-09-05 14:36:56 +0700

    crud tabel roles filament admin

---
+++ backend-gallery-web/app/Filament/Resources/RoleAccessResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RoleAccessResource\Pages;
+use App\Filament\Resources\RoleAccessResource\RelationManagers;
+use App\Models\RoleAccess;
+use App\Models\Roles;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RoleAccessResource extends Resource
+{
+    protected static ?string $model = Roles::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('role_name')
+                    ->label('Role Name')
+                    ->string()
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('handle_access')
+                    ->label('Handle Access')
+                    ->string()
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('role_name'),
+                TextColumn::make('handle_access'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRoleAccesses::route('/'),
+            'create' => Pages\CreateRoleAccess::route('/create'),
+            'edit' => Pages\EditRoleAccess::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getModelLabel(): string
+    {
+        return 'Role & Akses';
+    }
+}
