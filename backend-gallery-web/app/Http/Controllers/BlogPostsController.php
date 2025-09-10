4aa327b51c523cae4b8e3f6e0ccce60ed72cb559
Afaandii <fandisantuy140@gmail.com>
2025-09-10 20:02:23 +0700

    get data api in backend to frontend

---
+++ backend-gallery-web/app/Http/Controllers/BlogPostsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blogs;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class BlogPostsController extends Controller
+{
+    public function index()
+    {
+        $blog = Cache::remember('blog_all', now()->addMinutes(10), function () {
+            return Blogs::select('title', 'slug', 'excerpt', 'content', 'image', 'created_at')->get()->toArray();
+        });
+
+        if (!$blog) {
+            return json_encode([
+                'status' => 'Fail',
+                'message' => "Data post tidak ada",
+            ], 503);
+        }
+
+        return json_encode([
+            'status' => 'Ok',
+            'message' => 'Success get all data blog posts',
+            'blog' => $blog
+        ]);
+    }
+}
