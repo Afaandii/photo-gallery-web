51f7d94c59715b1a418f0abef1b1d25f66a8c08e
Afaandii <fandisantuy140@gmail.com>
2025-09-11 23:40:55 +0700

    adding skeleton loading for image post beranda

--- frontend-gallery-web/src/components/post/ImagePosts.jsx
+++ frontend-gallery-web/src/components/post/ImagePosts.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function ImagePosts() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -17,32 +18,46 @@ export default function ImagePosts() {
         setPosts(data.post);
       } catch (err) {
         console.log(err);
+      } finally {
+        setLoading(false);
       }
     };
     fetchPost();
   }, []);
 
   return (
-    <>
-      <section className="max-w-7xl mx-auto px-4 py-8">
-        <h1 className="text-[18px] font-bold mb-5">
-          Temukan lebih dari 1 juta gambar yang dibagikan atau diabadikan dari
-          beberapa orang.
-        </h1>
+    <section className="max-w-7xl mx-auto px-4 py-8">
+      <h1 className="text-[18px] font-bold mb-5">
+        Temukan lebih dari 1 juta gambar yang dibagikan atau diabadikan dari
+        beberapa orang.
+      </h1>
+
+      {loading ? (
+        // Skeleton load
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 animate-pulse">
+          {Array.from({ length: 6 }).map((_, idx) => (
+            <div
+              key={idx}
+              className="overflow-hidden rounded-xl shadow-lg bg-gray-200 h-[500px]"
+            ></div>
+          ))}
+        </div>
+      ) : (
+        // data
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {posts.map((post, index) => (
             <div key={index} className="overflow-hidden rounded-xl shadow-lg">
               <Link to={`/image-posts/${post.slug}`}>
                 <img
                   src={`http://localhost:8000/storage/${post.image}`}
-                  alt="Leaves with snow"
+                  alt={post.title || "image post"}
                   className="w-full h-[500px] object-cover"
                 />
               </Link>
             </div>
           ))}
         </div>
-      </section>
-    </>
+      )}
+    </section>
   );
 }
